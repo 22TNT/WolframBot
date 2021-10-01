3bd062f379acfda22ac6f14b601b31d6c18c74b5
tnt22@protonmail.com <tnt22@protonmail.com>
2021-10-01 13:37:27 +0700

    Release 1.0

--- wolfalpha.py
+++ wolfalpha.py
@@ -1,42 +1,40 @@
 import requests
-import json
 import discord
-import shutil
 from discord.ext import commands
+import shutil
 from PIL import Image
 import secret
 
 prefix = "="
-discord_token = secret.discord_token
-wolfram_token = secret.wolfram_token
-
 bot = commands.Bot(command_prefix=prefix)
 bot.remove_command("help")
 
 
-def send_request_to_api(request):
-    url = "http://api.wolframalpha.com/v1/simple?appid=" + wolfram_token + "&i=" + request + "&width=1000"
+def send_request_to_api(request, token):
+    url = "http://api.wolframalpha.com/v1/simple?appid=" + token + "&i=" + request + "&width=1000"
     response = requests.get(url, stream=True)
     if response.ok:
         with open("request.gif", "wb") as out_file:
             shutil.copyfileobj(response.raw, out_file)
-        im = Image.open('request.gif')
-        transparency = im.info.get('transparency')
-        im.save('request.png', transparency=transparency)
-        return 0
-    else:
-        return response.status_code
+    return response.status_code
+
+
+def convert_gif_to_png(filename):
+    im = Image.open(filename + '.gif')
+    im.save(filename + '.png', transparency=im.info.get('transparency'))
+    return 0
 
 
 @bot.command()
 async def query(ctx, *request):
     request = " ".join(request[:])
-    request = request.replace("+", "%2B")
-    if not send_request_to_api(request):
+    response = send_request_to_api(request.replace("+", "%2B"), secret.wolfram_token)
+    if response == 200:
+        convert_gif_to_png('request')
         file = discord.File("request.png", filename="request.png")
-        await ctx.send("Here's the result for `" + request.replace("%2B", "+") + "`", file=file)
+        await ctx.send("Here's the result for `" + request + "`", file=file)
     else:
-        await ctx.send("Something went wrong, try again or contact admin")
+        await ctx.send("Something went wrong, code = " + str(response))
 
 
 @bot.command()
@@ -44,4 +42,4 @@ async def help(ctx):
     e = discord.Embed(Title="Help", description="Use `=query <query>` to send a request")
     await ctx.send(embed=e)
 
-bot.run(discord_token)
+bot.run(secret.discord_token)
